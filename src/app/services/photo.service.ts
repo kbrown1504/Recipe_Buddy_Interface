7624ee96d81ba5755fe49ab9aa7ab259821dd5ec
kbrown1504 <keegqbrown@gmail.com>
2022-10-31 16:47:20 -0500

    added camera for mobile apps

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core'; 
+
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+//import { Preferences } from '@capacitor/preferences';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  private photo;
+
+  constructor() { }
+
+  public async takePhoto() {
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+    this.photo = capturedPhoto;
+  }
+}
