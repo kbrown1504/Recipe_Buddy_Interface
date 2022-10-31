4018e3f806a9d8c7bf0f21c2dfb3514141745f26
Solum510 <nathanrenard510@gmail.com>
2022-10-31 18:45:56 -0500

    added a little bit of funcionality to the find recipe button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PhotoService } from '../services/photo.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,7 @@ import { PhotoService } from '../services/photo.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  recipeList = [];
 
   constructor(private photoService: PhotoService) {}
 
@@ -15,4 +17,8 @@ export class HomePage {
     this.photoService.takePhoto();
   }
 
+  getRecipes() {
+    this.recipeList.push({name:"Roasted Chicken", url:"https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Country-Roasted-Chicken_EXPS_FT20_17623_F_0130_1.jpg"});
+    this.recipeList.push({name:"chicken Sandwhich", url:"https://upload.wikimedia.org/wikipedia/commons/1/1f/Chicken_salad_sandwich_01.jpg"});
+  }
 }
