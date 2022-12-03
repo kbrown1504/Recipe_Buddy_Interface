06b065f735073940db2c1bdd1a3ce1d5fc562dfe
Solum510 <nathanrenard510@gmail.com>
2022-12-03 17:16:27 -0600

    added recipe model and some recipes to the getRecipes method in the homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PhotoService } from '../services/photo.service';
 import { NavController } from '@ionic/angular';
+import { Recipe } from 'src/models/recipe';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  recipeList = [];
+  public recipeList: Recipe[] = [];
 
   constructor(private photoService: PhotoService) {}
 
@@ -18,7 +19,49 @@ export class HomePage {
   }
 
   getRecipes() {
-    this.recipeList.push({name:"Roasted Chicken", url:"https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Country-Roasted-Chicken_EXPS_FT20_17623_F_0130_1.jpg"});
-    this.recipeList.push({name:"chicken Sandwhich", url:"https://upload.wikimedia.org/wikipedia/commons/1/1f/Chicken_salad_sandwich_01.jpg"});
+    this.recipeList.push({name: "Spaghetti", 
+    url: "https://lilluna.com/wp-content/uploads/2018/05/easy-spaghetti-resize-2.jpg", 
+    ingredients: ["Spaghetti noodles", "Pasta Sauce"],
+    Steps: [
+      "Boil a pot of water",
+      "Put your Spaghetti noodles into the boiling water",
+      "Wait around 15 minutes",
+      "Strain your Spaghetti noodles and get rid of the water",
+      "Mix in your sauce with the Spaghetti noodles",
+      "Enjoy!"
+    ],
+  });
+
+  this.recipeList.push({
+    name: "Bacon and Eggs",
+    url: "https://i.imgur.com/fg9iEmZ.jpg",
+    ingredients: ["Bacon", "Eggs", "Seasonings as desired"],
+    Steps: [
+      "Using a large skillet, place slices of bacon in the pan over medium-high heat.",
+      "Cook your bacon, turning it every couple minutes unitl it reaches the crispiness you desire.",
+      "Drain the bacon on a plate layered with paper towels.",
+      "Reserve the bacon grease",
+      "Once bacon is cooked, turn your heat down to medium-low. Don't drain the bacon fat. You are going to use it all to fry your eggs.",
+      "Crack the eggs into the pan, and let them begin to cook for about 30-45 seconds.",
+      "Using a large spoon, start to baste the eggs with the bacon fat by scooping the fat over the tops of the eggs (you aren't going to flip these eggs)",
+      "After about three minutes, the eggs should look done. The whites will be firm and the yolk will be partially cooked.",
+      "Use a slotted spatula to remove the eggs from the pan, then blot each egg with a paper towel before putting it on the plate. So there is very little fat on the eggs.",
+      "Plate your eggs and bacon, season with salt and pepper as desired",
+      "Enjoy!"
+    ]
+  });
+
+  this.recipeList.push({
+    name: "Peanut Butter and Jelly Sandwich",
+    url: "https://storcpdkenticomedia.blob.core.windows.net/media/recipemanagementsystem/media/recipe-media-files/recipes/retail/x17/2018_grilled-peanut-butter-and-jelly_20336_760x580.jpg?ext=.jpg",
+    ingredients: ["Peanut Butter", "Jelly", "Sliced Bread"],
+    Steps: [
+      "Take out two pieces of sliced bread",
+      "Spread jelly on one slice of bread",
+      "Spread peanut butter on the other slice of bread",
+      "Put the two pieces of bread together",
+      "Enjoy!"
+    ]
+  });
   }
 }
---
+++ src/models/recipe.ts
@@ -0,0 +1,6 @@
+export class Recipe {
+    name: String;
+    url: String;
+    ingredients: String[];
+    Steps: String[];
+}
