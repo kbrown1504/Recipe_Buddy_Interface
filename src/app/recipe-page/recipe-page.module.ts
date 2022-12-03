9271f4c0775050da69ddd7ebd1587a50319f8fb2
Solum510 <nathanrenard510@gmail.com>
2022-12-03 17:28:23 -0600

    added start to recipe page

---
+++ src/app/recipe-page/recipe-page.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RecipePage } from './recipe.page';
+
+import { RecipeRoutingModule } from './recipe-page-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RecipeRoutingModule
+  ],
+  declarations: [RecipePage]
+})
+export class HomePageModule {}
---
+++ src/app/recipe-page/recipe.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+import { NavController } from '@ionic/angular';
+import { Recipe } from 'src/models/recipe';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'recipe.page.html',
+  styleUrls: ['recipe.page.scss'],
+})
+
+export class RecipePage{
+
+}
